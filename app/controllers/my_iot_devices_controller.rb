fb4160e5c2da0dd9a501141c794835272448efea
Prelang Builder <builder@prelang.com>
2016-10-06 16:56:34 +0000

    Added require_user_signed_in filters to scaffolded controller

--- app/controllers/my_iot_devices_controller.rb
+++ app/controllers/my_iot_devices_controller.rb
@@ -1,4 +1,8 @@
 class MyIotDevicesController < ApplicationController
+
+  #->Prelang (scaffolding:rails/scope_to_user)
+  before_filter :require_user_signed_in, only: [:new, :edit, :create, :update, :destroy]
+
   before_action :set_my_iot_device, only: [:show, :edit, :update, :destroy]
 
   # GET /my_iot_devices
