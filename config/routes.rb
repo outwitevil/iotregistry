df1d8106a58c57923ae766e7b00004a1d35cae1d
Prelang Builder <builder@prelang.com>
2016-10-06 16:56:49 +0000

    Set up member route for #vote action

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  resources :reviews
+  resources :reviews do
+  #->Prelang (voting/acts_as_votable)
+  member do
+    get "vote"
+  end
+end
+
 
   resources :device_categories
 
