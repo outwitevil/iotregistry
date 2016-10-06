78069663abdc38312ab9012fbbabb69b572c5092
Prelang Builder <builder@prelang.com>
2016-10-06 16:56:28 +0000

    Made model 'Firmware' votable by adding acts_as_votable

--- app/models/firmware.rb
+++ app/models/firmware.rb
@@ -2,4 +2,5 @@ class Firmware < ActiveRecord::Base
   belongs_to :iotdevice
   validates_formatting_of :download_url, using: :url
   validates_formatting_of :readme_url, using: :url
+  acts_as_votable 
 end
