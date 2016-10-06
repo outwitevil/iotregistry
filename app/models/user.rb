cb6e74511e7b4c09137a183ee0893412b0d223a1
Adam Lund <outwitevil@users.noreply.github.com>
2016-10-06 13:10:21 -0400

    Fixing associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ActiveRecord::Base
   devise :omniauthable, omniauth_providers: [:facebook]
 
   #->Prelang (user_login/devise)
-  has_many :iotdevices
+  has_many :iotdevices, through: :my_iot_devices
   def self.find_for_facebook_oauth(auth, signed_in_resource=nil)
     user = User.where(provider: auth.provider, uid: auth.uid).first
 
